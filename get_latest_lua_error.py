af391b37db17140d5d6b889e54de37f6c2f342fa
Unknown <tilic5@gmail.com>
2019-04-13 13:11:05 +0200

    also search for errors propagated from C code

--- get_latest_lua_error.py
+++ get_latest_lua_error.py
@@ -9,17 +9,22 @@ class GetLatestLuaErrorCommand(sublime_plugin.TextCommand):
 		list_of_files = glob.glob(os.getenv('APPDATA')+'\\Fatshark\\Vermintide 2\\console_logs\\*') # * means all if need specific format then *.csv
 		latest_file = max(list_of_files, key=os.path.getctime)
 		found_error = False
+		found_lua_stack = False
 		with open(latest_file, 'rb', 0) as file, \
 			mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
 			if s.find(b'<<Lua Error>>') != -1:
 				found_error = True
-		if found_error:
+			if s.find(b'<<Lua Stack>>') != -1:
+				found_lua_stack = True
+
+		if found_error or found_lua_stack:
+			error_regex = "<<Lua Error>>.*>>" if found_error else "<<Lua Stack>>.*$"
 			view = sublime.active_window().open_file(latest_file)
 			def find_in_file():
 				if view.is_loading():
 					sublime.set_timeout_async(find_in_file, 0.1)
 				else:
-					all_errors = view.find_all("<<Lua Error>>.*>>", 0)
+					all_errors = view.find_all(error_regex, 0)
 					if all_errors:
 						error_region = all_errors[-1]
 						view.show_at_center(error_region)
